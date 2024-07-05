9b03f5dcfaa37305df5137170cdc87bf5d4d2c9b
timothee-oc <timothee.oc@proton.me>
2024-07-05 10:59:43 +0200

    Add user attributes needed for the project

--- softdesk/authentication/models.py
+++ softdesk/authentication/models.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
-    pass
+    age = models.PositiveSmallIntegerField(validators=(MinValueValidator(15),))
+    can_be_contacted = models.BooleanField(blank=True, default=False)
+    can_data_be_shared = models.BooleanField(blank=True, default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
