e29bdee5f6980a4712ae351d130353e2353d2b73
timothee-oc <timothee.oc@proton.me>
2024-07-11 19:02:22 +0200

    Add rest framework as installed app and set language and timezone for France

--- softdesk/softdesk/settings.py
+++ softdesk/softdesk/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
     "authentication",
     "project",
     "contributor",
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
