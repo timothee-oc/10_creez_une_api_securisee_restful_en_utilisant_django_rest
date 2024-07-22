d1f24ced8ea06695ff0b066fb34e3ab522a2a38a
timothee-oc <tmth.oc@gmail.com>
2024-07-22 13:44:25 +0200

    handle password hashing when creating or updating user

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -4,4 +4,18 @@ from .models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "age", "can_be_contacted", "can_data_be_shared", "created_time"]
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data: dict):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
