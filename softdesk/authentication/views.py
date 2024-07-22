7c53cb6cdaa042bd2cd36dbb178c64f69ecba338
timothee-oc <tmth.oc@gmail.com>
2024-07-22 13:44:53 +0200

    No permissions should be required to create a user

--- softdesk/authentication/views.py
+++ softdesk/authentication/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import UserSerializer
 
@@ -7,3 +8,8 @@ class UserViewset(ModelViewSet):
 
     def get_queryset(self):
         return User.objects.all()
+    
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        return super().get_permissions()
