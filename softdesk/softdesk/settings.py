46149a8dd3ed1f3a411dcf2c6c7cd946390fba27
timothee-oc <tmth.oc@gmail.com>
2024-07-22 13:48:33 +0200

    register jwt app and define being authenticated as the default required permission

--- softdesk/softdesk/settings.py
+++ softdesk/softdesk/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "rest_framework_simplejwt",
     "authentication",
     "project",
     "contributor",
@@ -127,3 +128,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "authentication.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
