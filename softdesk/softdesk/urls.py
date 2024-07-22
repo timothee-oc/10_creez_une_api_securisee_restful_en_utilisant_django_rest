dfe7025bb85eb6d11adad78a3e6900ac7787ea53
timothee-oc <tmth.oc@gmail.com>
2024-07-22 13:49:23 +0200

    Use nested routers for issues and comments and add paths for token access and refresh

--- softdesk/softdesk/urls.py
+++ softdesk/softdesk/urls.py
@@ -1,33 +1,26 @@
-"""
-URL configuration for softdesk project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_nested import routers
 from authentication.views import UserViewset
 from project.views import ProjectViewset, IssueViewset, CommentViewset
 from contributor.views import ContributorViewset
 
 router = routers.SimpleRouter()
-
 router.register(prefix="users", viewset=UserViewset, basename="users")
 router.register(prefix="projects", viewset=ProjectViewset, basename="projects")
-router.register(prefix="issues", viewset=IssueViewset, basename="issues")
-router.register(prefix="comments", viewset=CommentViewset, basename="comments")
 router.register(prefix="contributors", viewset=ContributorViewset, basename="contributors")
 
+projects_router = routers.NestedSimpleRouter(router, "projects", lookup="project")
+projects_router.register(prefix="issues", viewset=IssueViewset, basename="project-issues")
+
+issues_router = routers.NestedSimpleRouter(projects_router, "issues", lookup="issue")
+issues_router.register(prefix="comments", viewset=CommentViewset, basename="issue-comments")
+
 urlpatterns = [
-    path("api/", include(router.urls))
+    path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(router.urls)),
+    path("api/", include(projects_router.urls)),
+    path("api/", include(issues_router.urls)),
 ]
