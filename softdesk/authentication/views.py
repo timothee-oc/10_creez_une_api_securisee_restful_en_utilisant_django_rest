49a0e347219c77586b3a6b75abcfacc9bc71db90
timothee-oc <tmth.oc@gmail.com>
2024-07-30 20:20:38 +0200

    Add a permission to prevent a user from updating or deleting another user account

--- softdesk/authentication/views.py
+++ softdesk/authentication/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsSelf
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -9,4 +10,6 @@ class UserViewset(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == "create":
             self.permission_classes = [permissions.AllowAny]
+        elif self.action in ("update", "partial_update", "destroy"):
+            self.permission_classes = [IsSelf]
         return super().get_permissions()
