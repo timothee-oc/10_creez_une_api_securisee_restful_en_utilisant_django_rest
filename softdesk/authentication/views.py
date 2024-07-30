e2d4a778e0fe5848d869a2e77ff68762a953c742
timothee-oc <tmth.oc@gmail.com>
2024-07-30 19:48:45 +0200

    Remove authentication permission to create a user

--- softdesk/authentication/views.py
+++ softdesk/authentication/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import User
 from .serializers import UserSerializer
 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = [permissions.AllowAny]
+        return super().get_permissions()
