1b349318b45c7ae16cc6a48c3fd6bdcf37d83d16
timothee-oc <tmth.oc@gmail.com>
2024-07-30 08:47:54 +0200

    remove contributor from project for now

--- softdesk/contributor/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Contributor
-
-class ContributorSerializer(ModelSerializer):
-    class Meta:
-        model = Contributor
-        fields = '__all__'
--- softdesk/contributor/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Contributor
-from .serializers import ContributorSerializer
-
-class ContributorViewset(ModelViewSet):
-    serializer_class = ContributorSerializer
-    queryset = Contributor.objects.all()
