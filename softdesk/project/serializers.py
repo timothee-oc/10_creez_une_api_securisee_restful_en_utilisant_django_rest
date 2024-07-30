21e613f20b19b648447d9d081d983787f6dd599a
timothee-oc <tmth.oc@gmail.com>
2024-07-30 20:58:34 +0200

    Author field of a project is automatically set to the user that send the request

--- softdesk/project/serializers.py
+++ softdesk/project/serializers.py
@@ -5,6 +5,12 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+        extra_kwargs = {"author": {"read_only": True}}
+    
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["author"] = request.user
+        return super().create(validated_data)
 
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
