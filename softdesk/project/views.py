246770ad1b314c272b900f1bb227a58840abc974
timothee-oc <tmth.oc@gmail.com>
2024-07-30 21:13:24 +0200

    Prevent user from updating or deleting a project that they are not author of

--- softdesk/project/views.py
+++ softdesk/project/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
 from .models import Project, Issue, Comment
 from .serializers import ProjectSerializer, IssueSerializer, CommentSerializer
+from .permissions import IsAuthor
 
 class ProjectViewset(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+    def get_permissions(self):
+        if self.action in ("update", "partial_update", "destroy"):
+            self.permission_classes = [IsAuthor]
+        return super().get_permissions()
 
 class IssueViewset(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
