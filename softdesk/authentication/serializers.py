9e187f4f22298c0b72a5b91f3a812899f128b457
timothee-oc <tmth.oc@gmail.com>
2024-07-30 20:34:40 +0200

    Make sure to update password if user update it

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -12,3 +12,11 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        password = validated_data.get("password")
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
