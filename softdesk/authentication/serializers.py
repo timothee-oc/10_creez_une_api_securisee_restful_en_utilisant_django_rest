1acd3e121897ea12a2ac4c7a542a61c4b749f89d
timothee-oc <tmth.oc@gmail.com>
2024-07-30 20:02:13 +0200

    Hash password during user creation

--- softdesk/authentication/serializers.py
+++ softdesk/authentication/serializers.py
@@ -5,3 +5,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
