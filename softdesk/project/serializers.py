7d0d6377d44417832804ba34a9b988d272b5d6ea
timothee-oc <tmth.oc@gmail.com>
2024-07-30 22:18:13 +0200

    Automatically set author and project foreign keys of issue created

--- softdesk/project/serializers.py
+++ softdesk/project/serializers.py
@@ -16,6 +16,18 @@ class IssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
         fields = '__all__'
+        extra_kwargs = {
+            "author": {"read_only": True},
+            "project": {"read_only": True},
+        }
+    
+    def create(self, validated_data):
+        request = self.context.get("request")
+        view = self.context.get("view")
+        project = Project.objects.get(id=view.kwargs.get("project_pk"))
+        validated_data["author"] = request.user
+        validated_data["project"] = project
+        return super().create(validated_data)
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
