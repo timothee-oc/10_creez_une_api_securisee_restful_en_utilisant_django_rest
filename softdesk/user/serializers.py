e17d8a5e4da03ce308c5ee3bcaa5b38538e3215c
timothee-oc <tmth.oc@gmail.com>
2025-02-27 12:55:55 +0100

    set personal data of user as write only

--- softdesk/user/serializers.py
+++ softdesk/user/serializers.py
@@ -6,7 +6,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'password', 'age', 'can_be_contacted', 'can_data_be_shared']
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True},
+            'age': {'write_only': True},
+            'can_be_contacted': {'write_only': True},
+            'can_data_be_shared': {'write_only': True},
         }
     
     def validate_age(self, value):
