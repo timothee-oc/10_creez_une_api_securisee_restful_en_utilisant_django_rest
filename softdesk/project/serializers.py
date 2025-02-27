f752bff824f4476a14cc2e3bc4a1216fe4e0c8d0
timothee-oc <tmth.oc@gmail.com>
2025-02-27 12:12:36 +0100

    add user field as read only in contributor serializer

--- softdesk/project/serializers.py
+++ softdesk/project/serializers.py
@@ -11,7 +11,7 @@ class ContributorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contributor
         fields = '__all__'
-        read_only_fields = ['project', 'role', 'author']
+        read_only_fields = ['user', 'project', 'role', 'author']
 
 class IssueSerializer(serializers.ModelSerializer):
     class Meta:
