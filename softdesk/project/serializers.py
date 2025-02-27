bb539f41499b823fe85da083e468dd99f55c298a
timothee-oc <tmth.oc@gmail.com>
2025-02-27 12:55:08 +0100

    Prevent from assigning an issue to a non-contributor user

--- softdesk/project/serializers.py
+++ softdesk/project/serializers.py
@@ -18,6 +18,14 @@ class IssueSerializer(serializers.ModelSerializer):
         model = Issue
         fields = '__all__'
         read_only_fields = ['author', 'created_time', 'project']
+    
+    def validate_assignee(self, value):
+        project_id = self.context.get('request').parser_context.get('kwargs').get('project_pk')
+
+        if value and not Contributor.objects.filter(project=project_id, user=value).exists():
+            raise serializers.ValidationError("L'utilisateur assigné doit être contributeur du projet")
+        
+        return value
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
